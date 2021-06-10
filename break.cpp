0330f477582b1f298430d1e694a0636e0b5ebfa1
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2021-06-11 00:57:14 +0530

    Add files via upload

---
+++ break.cpp
@@ -0,0 +1,23 @@
+/*break statement  is used to terminate   the loop
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter a numebr"<<endl;
+    cin>>n;
+    for(int i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            cout<<"not a prime number \n";
+            break;
+        }
+        
+        }
+        if
+        {
+            cout <<"it is a prime number ";
+        }
+}
