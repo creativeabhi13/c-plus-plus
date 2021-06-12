6ec116e680356c7af733d050269b29d414138aea
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2021-06-12 14:43:29 +0530

    basic

---
+++ array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    int n;
+    int a[50];
+    cin>>n;
+    for(int i=0;i<n;i++){
+          cin>>a[i];
+          cout<<a[i]<<endl;
+    }
+}
