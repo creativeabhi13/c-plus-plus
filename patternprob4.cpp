15e187e9ee68621c8e2890bed8a4665a24cc5df0
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2021-06-20 20:15:12 +0530

    baisc

--- patternprob4.cpp
+++ patternprob4.cpp
@@ -6,7 +6,7 @@
       ****
      *****
     ****** */
-  /* #include<iostream>
+/* #include<iostream>
   using namespace std;
   int main()
   {
@@ -31,18 +31,82 @@
   }
  */
 
-
-
-
- /* Print Half Pyramid using numbers
+/* Print Half Pyramid using numbers
 
  1
- 1 2
- 1 2 3
- 1 2 3 4
- 1 2 3 4 5  
+ 2 2
+ 3 3 3
+ 4 4 4 4
+ 5 5 5 5 5  
  */
-
-
-
- 
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter the number ";
+    cin >> n;
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 1; j <= i; ++j)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
+*/
+/*
+Print Half Pyramid using numbers
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4 5 6
+1 2 3 4 5 6 7
+*/
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter the number ";
+    cin >> n;
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 1; j <= i; ++j)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+*/
+/*
+Print Inverted Half Pyramid using numbers
+1 2 3 4 5 6 
+1 2 3 4 5
+1 2 3 4 
+1 2 3
+1 2 
+1 
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter the number ";
+    cin >> n;
+    for (int i = n; i >= 1; --i)
+    {
+        for (int j = 1; j <= i; ++j)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
