788bf2afcffe7d64d8ec21cca3edd013ff68a1bf
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2021-09-05 10:39:01 +0530

    basic

---
+++ arrayprob.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+}
