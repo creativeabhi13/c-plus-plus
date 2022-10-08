9940ad772ec0150a889a509664db93f6faa4ecdb
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2022-10-08 22:29:55 +0530

    bin search

--- Dsa/binsearch.cpp
+++ Dsa/binsearch.cpp
@@ -4,45 +4,57 @@ Binary  search
 */
 #include <iostream>
 using namespace std;
-int binsearch(int arr[], int size, int key)
+
+int binarySearch(int arr[], int size, int key)
 {
+
     int start = 0;
     int end = size - 1;
-    int mid = (start + mid) / 2;
+    // suppose the start is 2^31-1 +@^31-1/2->it will be more so that's the reason we are using this technique to
+    int mid = start + (end - start) / 2;
+
     while (start <= end)
     {
+
         if (arr[mid] == key)
         {
             return mid;
         }
-        // go to right side of the array
+
+        // go to right wala part
         if (key > arr[mid])
         {
             start = mid + 1;
         }
         else
-        {
+        { // key < arr[mid]
             end = mid - 1;
         }
-        mid = (start + end) / 2;
+
+        mid = start + (end - start) / 2;
     }
+
     return -1;
 }
+
 int main()
 {
-    int n;
-    cout << "Enter the number of element in the array" << endl;
+    int n, key;
+    cout << "Enter the size of the array" << endl;
     cin >> n;
     int a[n];
+
+    cout << "Enter the element in the array" << endl;
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
-    cout << "Enter the key element" << endl;
-    int key;
+    cout << "Enter the key" << endl;
     cin >> key;
-    cout << "key is at index" << endl;
 
-    int s = binsearch(a, n, key);
-    cout << " " << s << endl;
+    int Index = binarySearch(a, n, key);
+
+    cout << " Index of  is " << Index << endl;
+
+    return 0;
 }
