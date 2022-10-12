534208a766bb0231a534c44df95846ec7259b3fc
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2022-10-12 18:49:26 +0530

    pivot

---
+++ Dsa/pivot.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int pivot(int arr[], int n)
+
+{
+
+    int s = 0;
+    int e = n - 1;
+    int mid = s + (e - s) / 2;
+
+    while (s < e)
+    {
+        if (arr[mid] >= arr[0])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return s;
+}
+int main()
+{
+
+    int n;
+    cout << "Enter the size of the array" << endl;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    cout << "The pivot element is " << pivot(a, n);
+}
